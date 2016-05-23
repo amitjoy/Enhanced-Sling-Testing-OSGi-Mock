08a0743884c4126de48bf3d7f19e0d587b090f1b
Amit Kumar Mondal <admin@amitinside.com>
2016-05-24 00:05:30 +0200

    StringUtil license changed
    Signed-off-by: Amit Kumar Mondal <admin@amitinside.com>

--- com.amitinside.sling.testing.osgi-mock/src/main/java/com/amitinside/sling/testing/osgi/mock/StringUtil.java
+++ com.amitinside.sling.testing.osgi-mock/src/main/java/com/amitinside/sling/testing/osgi/mock/StringUtil.java
@@ -1,14 +1,17 @@
 /**
- * Copyright (c) 2011, 2016 Eurotech and/or its affiliates
- *
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *   Eurotech
- *   Amit Kumar Mondal (admin@amitinside.com)
+* Copyright [2016] [Amit Kumar Mondal]
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
  */
 package com.amitinside.sling.testing.osgi.mock;
 
@@ -16,7 +19,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class StringUtil {
+public final class StringUtil {
 
 	public static boolean equals(final CharSequence cs1, final CharSequence cs2) {
 		if (cs1 == cs2) {
